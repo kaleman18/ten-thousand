b7f563c471442be707b5427e735ba92eaad1e44e
kaleman18 <kylealeman18@gmail.com>
2024-01-18 17:19:36 -0800

    finished the lab minus the tests

--- ten_thousand/game.py
+++ ten_thousand/game.py
@@ -1,13 +1,14 @@
 from game_logic import GameLogic
+from collections import Counter
 
 round_tracker = 1
 dice_remaining = 6
 total_score = 0
 unbanked_points = 0
 previous_score = 0
-
-def play():
-    start()
+response_checker = 0
+response = ""
+dice_checker = None
 
 
 def start():
@@ -16,7 +17,7 @@ def start():
     response = input("> ")
 
     if response == "y":
-        roll_dice()
+        roll_dice_start()
 
     if response == "n":
         print("Okay! Maybe another time.")
@@ -28,83 +29,146 @@ def reroll():
     global unbanked_points
     global total_score
     global round_tracker
+    global response
+    global dice_checker
 
     print(f"Rolling {dice_remaining} dice...")
 
-    GameLogic.roll_dice(dice_remaining)
-    print(f"Current Unbanked Points = {unbanked_points}")
-    print(f"Total Score = {total_score}")
-    print("Enter dice to keep, or (q)uit:")
+    dice_checker = GameLogic.roll_dice(dice_remaining)
+    
+    check_for_cheater_and_roll_dice()
 
-    response = input("> ")
+def roll_dice_start():
+    global dice_remaining
+    global unbanked_points
+    global total_score
+    global round_tracker
+    global previous_score
+    global response
+    global dice_checker
 
-    if response == "q":
-        print(f"Thanks for playing. You earned {total_score} points")
+    print(f"Starting round {round_tracker}")
 
-    else:
-        saved_dice = list(response)
-        saved_dice = [int(i) for i in saved_dice]
-        dice_remaining = dice_remaining - len(saved_dice)
-        unbanked_points += GameLogic.calculate_score(saved_dice)
-        print(f"You have {unbanked_points} unbanked points and {dice_remaining} dice remaining")
-        print("(r)oll again, (b)ank your points or (q)uit:")
-        response = input("> ")
-        
-        if response == "r":
-            reroll()
-        if response == "b":
-            dice_remaining = 6
-            total_score += unbanked_points
-            unbanked_points = 0
-            round_tracker += 1
-            roll_dice()
-        if response == "q":
-             print(f"Thanks for playing. You earned {total_score} points")
-             return
+    print(f"Rolling {dice_remaining} dice...")
+
+    dice_checker = GameLogic.roll_dice(dice_remaining)
+
+    check_for_cheater_and_roll_dice()
 
 def roll_dice():
+    global dice_remaining
+    global unbanked_points
+    global total_score
+    global round_tracker
+    global previous_score
+    global response
+    global dice_checker
+
+    print(f"You banked {previous_score} points in round {round_tracker}")
+
+    previous_score = 0
+
+    print(f"Total score is {total_score} points")
+
+    round_tracker += 1
+
+    print(f"Starting round {round_tracker}")
+
+    print(f"Rolling {dice_remaining} dice...")
+
+    dice_checker = GameLogic.roll_dice(dice_remaining)
+
+    check_for_cheater_and_roll_dice()
+
+def check_for_cheater_and_roll_dice():
 
     global dice_remaining
     global unbanked_points
     global total_score
     global round_tracker
     global previous_score
-    print(f"You banked {previous_score} points in round {round_tracker - 1}")
+    global response
+    global dice_checker
 
-    previous_score = 0
+    print("*** ", dice_checker," ***")
 
-    print(f"Starting round {round_tracker}")
-
-    print(f"Total Score = {total_score}")
-
-    print(f"Rolling {dice_remaining} dice...")
-
-    GameLogic.roll_dice(dice_remaining)
+    if GameLogic.calculate_score(dice_checker) == 0:
+        print("****************************************")
+        print("**        Zilch!!! Round over         **")
+        print("****************************************")
+        dice_remaining = 6
+        unbanked_points = 0
+        roll_dice()
 
     print("Enter dice to keep, or (q)uit:")
-
+    response == ""
     response = input("> ")
-
     if response == "q":
         print(f"Thanks for playing. You earned {total_score} points")
+        return
+    response = list(response)
+    response = [int(i) for i in response]
 
-    else:
-        saved_dice = list(response)
-        saved_dice = [int(i) for i in saved_dice]
-        dice_remaining = dice_remaining - len(saved_dice)
-        unbanked_points += GameLogic.calculate_score(saved_dice)
+    #anti cheat
+    if len(response) > dice_remaining:
+        print("Cheater!!! Or possibly made a typo...")
+        check_for_cheater_and_roll_dice()
+        
+    dice_rolled_dict = Counter(dice_checker)
+
+    for num in response:
+        if num not in dice_rolled_dict :
+            print("Cheater!!! Or possibly made a typo...")
+            check_for_cheater_and_roll_dice()
+
+        if num in dice_rolled_dict:
+            dice_rolled_dict[num] -= 1
+
+        if num in dice_rolled_dict and dice_rolled_dict[num] == -1:
+            print("Cheater!!! Or possibly made a typo...") 
+            check_for_cheater_and_roll_dice()
+
+    if len(response) == 6:
+        unbanked_points += GameLogic.calculate_score(response)
         print(f"You have {unbanked_points} unbanked points and {dice_remaining} dice remaining")
         print("(r)oll again, (b)ank your points or (q)uit:")
+        response == ""
         response = input("> ")
         
         if response == "r":
             reroll()
+
         if response == "b":
             dice_remaining = 6
             total_score += unbanked_points
             previous_score += unbanked_points
             unbanked_points = 0
-            round_tracker += 1
+            roll_dice()
+
+        if response == "q":
+             print(f"Thanks for playing. You earned {total_score} points")
+             return
+        
+        check_for_cheater_and_roll_dice()
+
+    
+    else:
+        
+        dice_remaining = dice_remaining - len(response)
+        unbanked_points += GameLogic.calculate_score(response)
+        print(f"You have {unbanked_points} unbanked points and {dice_remaining} dice remaining")
+        print("(r)oll again, (b)ank your points or (q)uit:")
+        response == ""
+        response = input("> ")
+        
+        if response == "r":
+            reroll()
+
+        if response == "b":
+            dice_remaining = 6
+            total_score += unbanked_points
+            previous_score += unbanked_points
+            unbanked_points = 0
             roll_dice()
 
         if response == "q":
@@ -112,18 +176,6 @@ def roll_dice():
              return
 
 
-        
-
-
-
-        
-
-
-
-
-    
-    
 if __name__ == "__main__":
-    play()
-
+    start()
 
